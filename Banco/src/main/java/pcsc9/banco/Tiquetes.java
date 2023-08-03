ecfb36448f1eb53f16291c716d1968edd41c0b4c
Daniel <dbarquero70479@ufide.ac.cr>
2023-08-02 20:36:09 -0600

    avance modulo 1.3 (comentado)

--- Banco/src/main/java/pcsc9/banco/Tiquetes.java
+++ Banco/src/main/java/pcsc9/banco/Tiquetes.java
@@ -7,13 +7,19 @@ package pcsc9.banco;
 import java.util.PriorityQueue;
 
 /**
- *
+ *Daniel
  * @author diego
  */
 public class Tiquetes {
- private PriorityQueue<Tiquete> colaPreferencial;
+    private PriorityQueue<Tiquete> colaPreferencial;
     private PriorityQueue<Tiquete> colaUnSoloTramite;
     private PriorityQueue<Tiquete> colaDosOMasTramites;
+    
+//    private PriorityQueue<Tiquete> caja1 = new PriorityQueue<>();   //creacion e cajas
+//    private PriorityQueue<Tiquete> caja2 = new PriorityQueue<>();   //tipo B (dos o mas transmites)
+//    private PriorityQueue<Tiquete> caja3 = new PriorityQueue<>();
+    
+
 
     public Tiquetes() {
         colaPreferencial = new PriorityQueue<>();
@@ -22,12 +28,15 @@ public class Tiquetes {
     }
 
     public void agregarTiquete(Tiquete tiquete) {
-        if (tiquete.getTipo().equals("P")) {
+        if (tiquete.getTipo().equals("P")) {        //preferencial
             colaPreferencial.offer(tiquete);
-        } else if (tiquete.getTipo().equals("A")) {
+        } else if (tiquete.getTipo().equals("A")) { //rapida
             colaUnSoloTramite.offer(tiquete);
-        } else if (tiquete.getTipo().equals("B")) {
+        } else if (tiquete.getTipo().equals("B")) { //dos o mas
             colaDosOMasTramites.offer(tiquete);
+            
+            llenarColas(tiquete);                         //llamar llenarcolas()
+            
         }
     }
 
@@ -42,4 +51,22 @@ public class Tiquetes {
             return null;
         }
     }
+
+//    public void llenarColas(Tiquete tiquete){   //pone los tiquets en caja al de menor espacio en cola
+//    
+//        int cantidadCaja1 = caja1.size();   //determina cantidad de objetos en caja
+//        int cantidadCaja2 = caja2.size();
+//        int cantidadCaja3 = caja3.size();
+//
+//        if(cantidadCaja1 <= cantidadCaja2 && cantidadCaja1 <= cantidadCaja3){       //caja 1 menor carga que las otras dos
+//          caja1.offer(tiquete);
+//
+//        }else if(cantidadCaja2 <= cantidadCaja1 && cantidadCaja2 <= cantidadCaja3){ //caja 2 menor carga que las otras dos
+//           caja2.offer(tiquete);
+//
+//        }else{                                                                      //caja 3 era el que menos carga tenia
+//            caja3.offer(tiquete);
+//        }  
+//   }
+    
 }    
