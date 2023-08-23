426914b3f56cea5bfbe87b736985228af00483c6
soporte <soporte@DESKTOP-D98L3JG>
2023-08-22 18:39:20 -0600

    agregar Reportes y llenar el reporte final

---
+++ Banco/src/main/java/pcsc9/banco/Reportes.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pcsc9.banco;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+        
+public class Reportes{
+    
+    public void LeerCajas(){
+        String rutaArchivo = "C:\\caja1.txt"; // Cambia esto a la ruta correcta
+
+        try {
+            FileReader fileReader = new FileReader(rutaArchivo);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String linea;
+            while ((linea = bufferedReader.readLine()) != null) {
+                System.out.println(linea);
+            }
+
+            bufferedReader.close();
+        } catch (IOException e) {
+            System.err.println("Error al abrir el archivo"+ e.getMessage());
+        }
+    }
+}
